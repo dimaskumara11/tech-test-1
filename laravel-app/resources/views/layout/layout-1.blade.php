9fe8e58fe87e22e2b6b3809d0d619d890eeb0c02
dimaskumara11 <dimaskumara11@gmail.com>
2024-11-12 08:38:46 +0700

    add setting background and logo

--- laravel-app/resources/views/layout/layout-1.blade.php
+++ laravel-app/resources/views/layout/layout-1.blade.php
@@ -20,13 +20,18 @@
     @stack('css_bottom')
 </head>
 
-<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+@php
+    $getTheme = \App\Models\ThemeSetting::first();
+@endphp
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
     <div class="wrapper">
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__wobble" src="{{ url('/assets/template-admin/') }}/dist/img/AdminLTELogo.png"
-                alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__wobble"
+                src="{{ $getTheme->logo ?: "url('/assets/template-admin/dist/img/AdminLTELogo.png')" }}"
+                alt="APPLogo" height="60" width="60">
         </div>
 
         <!-- Navbar -->
@@ -37,7 +42,7 @@
         @include('layout.partials.sidebar')
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
+        <div class="content-wrapper" style="background-image: url('{{ $getTheme->background }}') !important; background:size: cover">
             <!-- Content Header (Page header) -->
             <div class="content-header">
                 <div class="container-fluid">
--- laravel-app/resources/views/layout/partials/sidebar.blade.php
+++ laravel-app/resources/views/layout/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url("/dashboard") }}" class="brand-link">
-        <img src="{{ url('/assets/template-admin/') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+        <img src="{{ $getTheme->logo ?: "url('/assets/template-admin/dist/img/AdminLTELogo.png')" }}" alt="APP Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">APP</span>
     </a>
